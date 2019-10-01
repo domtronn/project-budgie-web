3bbae98244dfa39bc54711a1498227e804a8fb84
Douglas Roper <dougdroper@gmail.com>
2019-10-01 16:38:34 +0100

    Make fire 🔥

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,18 +1,15 @@
 import * as functions from "firebase-functions";
 import next from "next";
-
 const dev = process.env.NODE_ENV !== "production";
 
 const app = next({
     dev,
-    conf: { distDir: "dist/client" },
-    // dir: "./src/client",
+    conf: { distDir: "next" },
 });
-
 
 const handle = app.getRequestHandler();
 
 exports.nextjs = functions.https.onRequest((request, response) => {
-    console.log("File: " + request.originalUrl);
+    console.log("dtest File: " + request.originalUrl);
     return app.prepare().then(() => handle(request, response));
 });
