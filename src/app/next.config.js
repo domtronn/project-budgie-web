86ecb545ede9ad482a23cc4174f3ae885bae7fdb
domtronn <dominic.charlesworth@rvu.co.uk>
2019-10-26 16:14:21 +0100

    Power firebase by env file

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,3 +1,19 @@
+/* next.config.js */
+const webpack = require('webpack')
+require('dotenv').config()
+
 module.exports = {
-    distDir: "../../dist/functions/next"
-};
+  distDir: '../../dist/functions/next',
+  webpack: config => {
+    const env = Object
+      .keys(process.env)
+      .reduce((acc, it) => ({
+        ...acc,
+        [`process.env.${it}`]: JSON.stringify(process.env[it])
+      }), {})
+
+    config.plugins.push(new webpack.DefinePlugin(env))
+
+    return config
+  }
+}
